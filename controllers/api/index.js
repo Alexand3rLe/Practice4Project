90c5a8de93c7d5d429ebc107e8bcea5181218676
Alexander_L <alexander.w.leyva@gmail.com>
2024-07-25 14:15:02 -0500

    Post and Get requests

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
 const Task = require('../../models/Task');
 
+router.get('/', async(req, res) => {
+    try{
+        const allTasks = await Task.findAll()
+
+        res.status(200).json(allTasks);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
+})
+
 // localhost:3001/api
 router.post('/', async (req, res) => {
-    const newTask = Task.create({
-        name: 'Feed Pikachu', 
-        description: 'Feed Pikachu he is hungry',
-        status: 'todo'
+
+    const newTask = await Task.create({
+        name: req.body.name, 
+        description: req.body.description,
+        status: req.body.status
     });
 
     res.json(newTask);
